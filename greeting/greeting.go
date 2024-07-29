da9e6bc9d85925c6ec1e6bcc116787a0603a01fb
takuya11111 <takuya.iida@g.pascalia.co.jp>
2024-07-29 15:00:55 +0900

    first commit

---
+++ greeting/greeting.go
@@ -0,0 +1,11 @@
+// Package name: must be same with folder name *greeting*
+package greeting
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello!")
+}
+func SayGoodbye() {
+	fmt.Println("Goodbye!")
+}
